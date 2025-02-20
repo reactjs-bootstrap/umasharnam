baf3e633000a850a890ef416c5d911e6532da33f
Bhargav Patel <bhargavkachhadiya@gmail.com>
2025-02-20 12:47:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Users from "./Users";
+import CreateUser from "./CreateUser";
+import UpdateUser from "./UpdateUser";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />}></Route>
+          <Route path="/create" element={<CreateUser />}></Route>
+          <Route path="/update" element={<UpdateUser />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
